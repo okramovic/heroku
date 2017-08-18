1e8d2c25ea313627fbd9a51dd83d3b7dcd13beeb
marko <okram@protonmail.ch>
2017-08-19 00:45:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", function (request, response) {
 //	response.sendFile('public/index.html');
 //	response.sendFile('/index.html');
 //	response.sendFile('./index.html');
-	response.sendFile('index.html');
+	response.sendFile('/index.html');
 	response.end();
 });
 
