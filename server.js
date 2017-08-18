99d59612d22d68a9b5aef9f706bee2be32145d9f
marko <okram@protonmail.ch>
2017-08-19 00:33:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 console.log("hello node");
 
-var path = require('path');
+var path = require('Path');
 var express = require('express');
 var app = express();
 
@@ -15,7 +15,8 @@ for (var i=0;i<6;i++){
 app.get("/", function (request, response) {
 	//response.send('ohmg');
 
-	response.sendFile(__dirname + '/index.html');
+//	response.sendFile(__dirname + '/index.html');
+		response.sendFile(__dirname + 'index.html');
 //	response.sendFile('public/index.html');
 //	response.sendFile('/index.html');
 //	response.sendFile('./index.html');
